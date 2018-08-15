874f00ba4627d3565139e43ba2f904c16c6b6c1c
Nurbol Alpysbayev <anurbol@gmail.com>
2018-08-15 21:27:06 +0600

    .

---
+++ test/_triage.ts
@@ -0,0 +1,20 @@
+// todo move all ideas to this file, converting them to test messages
+// todo triage all tests
+
+describe('triage tests and delete me', () => {
+
+    it('______', () => {
+        throw new Error('______')
+    })
+
+
+
+
+
+
+
+
+
+    throw new Error('triage tests and delete me')
+})
+
